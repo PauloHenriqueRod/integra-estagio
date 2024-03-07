a6c6f830bf6611551612f39b7662542cd82a53dd
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2024-03-07 13:46:14 -0300

    Initial commit

---
+++ codigo_5.py
@@ -0,0 +1,18 @@
+'''
+5) Escreva um programa que inverta os caracteres de um string.
+
+
+IMPORTANTE:
+
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente 
+definida no código;
+
+b) Evite usar funções prontas, como, por exemplo, reverse;
+'''
+
+def reverte_string(frase: str) -> str:
+    return frase[::-1]
+
+while True:
+    frase = input('Frase que deseja revertar [digite "sair" para fechar]: ')
+    print(reverte_string(frase=frase))
